6d0bc4620ae30d603d66e064b2260377c12ce276
Marco Salvador <marcosalvador@MacBook-Air-di-Marco.local>
2024-06-18 12:22:48 +0200

    Move rapporto.C to Codici

---
+++ Codici/rapporto.C
@@ -0,0 +1,66 @@
+//Questo programma calcola il rapporto tra gli integrali di alta e bassa risoluzione
+//per ogni canale dalle coincidenze 1-2-3-4, escludendo i valori nulli
+
+#include "Riostream.h"
+#include <string>
+
+using namespace std;
+
+void rapporto() {
+    // Nome del file per lo stream di dati
+    string file_name = "AqqTrigger1234data0506ora1744.txt";
+    // Inizializza e apri il file di input
+    ifstream file_in(file_name);
+ 
+    // Verifica che il file sia aperto
+    if (!file_in.is_open()) {
+        cout << "Cannot open data file!" << endl;
+        return;
+    }
+    
+    // Variabili nel file da leggere
+    double q1_l, q1_h, q2_l, q2_h, q3_l, q3_h, q4_l, q4_h, t1, t2, t3, t4, clock;
+    
+    // Variabili per il rapporto
+    double q1_sum = 0, q2_sum = 0, q3_sum = 0, q4_sum = 0;
+    
+    // Contatore per il numero totale di candidati
+    int icand = 0;
+    
+    // Loop fino alla fine del file, riga per riga
+    while (file_in >> q1_l >> q1_h >> q2_l >> q2_h >> q3_l >> q3_h >> q4_l >> q4_h >> t1 >> t2 >> t3 >> t4 >> clock) {
+
+        // Ignora le righe con valori nulli
+        if (q1_l == 0 || q2_l == 0 || q3_l == 0 || q4_l == 0 || t1 == 0 || t2 == 0 || t3 == 0 || t4 == 0) {
+            continue;
+        }
+
+        // Aggiungi ai sommatori
+        q1_sum += q1_h / q1_l;
+        q2_sum += q2_h / q2_l;
+        q3_sum += q3_h / q3_l;
+        q4_sum += q4_h / q4_l;
+
+        // Incrementa il contatore per ogni riga letta
+        ++icand;
+    }
+    
+    // Chiudi il file di input
+    file_in.close();
+    
+    // Calcola i rapporti medi
+    double q1 = q1_sum / icand ;
+    double q2 = q2_sum / icand ;
+    double q3 = q3_sum / icand ;
+    double q4 = q4_sum / icand ;
+    
+    // Stampa i risultati
+    cout << fixed << setprecision(3);
+    cout << "Rapporto 1: " << q1 << endl;
+    cout << "Rapporto 2: " << q2 << endl;
+    cout << "Rapporto 3: " << q3 << endl;
+    cout << "Rapporto 4: " << q4 << endl;
+    cout << "Numero di candidati: " << icand << endl;
+    
+    return;
+}
