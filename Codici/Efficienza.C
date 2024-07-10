27b6027a123886968cac7deddae620d32e0f95ef
Davide Fuligno <dfuligno@cli-10-110-6-92.WIFIeduroamSTUD.units.it>
2024-07-10 15:20:26 +0200

    errore associato all'efficienza

--- Codici/Efficienza.C
+++ Codici/Efficienza.C
@@ -24,7 +24,7 @@ void Efficienza() {
     
     // Contatore per il numero totale di eventi
     int nEventi = 0;
-    int eff2 = 0, eff3 = 0;
+    int eff3 = 0;
     
     // Loop fino alla fine del file, riga per riga
     while (file_in >> q1_l >> q1_h >> q2_l >> q2_h >> q3_l >> q3_h >> q4_l >> q4_h >> t1 >> t2 >> t3 >> t4 >> clock) {
@@ -32,10 +32,6 @@ void Efficienza() {
         // Ignora le righe con valori nulli
         if (q1_l == 0 || q4_l == 0 || t1 == 0 || t4 == 0) {
             continue;
-        }
-        // guarda se lo scintillatore 2 ha visto un evento
-        if (q2_h != 0 && t2 != 0){
-            eff2 +=1;
         }
         // guarda se lo scintillatore 3 ha visto un evento
         if (q3_h != 0 && t3 != 0){
@@ -45,18 +41,16 @@ void Efficienza() {
         // Incrementa il contatore per ogni riga letta
         ++nEventi;
     }
-    cout << eff2 << "\t" << eff3 << "\t" << nEventi << endl;
     // Chiudi il file di input
     file_in.close();
     
     // Calcola le efficienze
-    double ef2 = eff2*1. / nEventi ;
     double ef3 = eff3*1. / nEventi ;
+    double err = sqrt((ef3*(1-ef3))/nEventi);
     
     // Stampa i risultati
-    cout << fixed << setprecision(3);
-    cout << "Efficienza 2: " << ef2 << endl;
-    cout << "Efficienza 3: " << ef3 << endl;
+    cout << fixed << setprecision(4);
+    cout << "Efficienza 3: " << ef3 << "\t sigma: " << err << endl;
     
     return;
 }
