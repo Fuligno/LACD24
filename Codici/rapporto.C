6953ec7b1d5b94bbd93e4c914ab52a80de677317
Viviana Poli <fabiopoli@cli-10-110-8-150.WIFIeduroamSTUD.units.it>
2024-07-11 14:07:56 +0200

    Update files to match local directory

--- Codici/rapporto.C
+++ Codici/rapporto.C
@@ -1,14 +1,15 @@
-//Questo programma calcola il rapporto tra gli integrali di alta e bassa risoluzione
-//per ogni canale dalle coincidenze 1-2-3-4, escludendo i valori nulli
-
-#include "Riostream.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
-void rapporto(double &r1, double &r2, double &r3, double &r4) {
+void Rapporto() {
+    double r1, r2, r3, r4;
     // Nome del file per lo stream di dati
     string file_name = "../Dati/AqqTrigger1234data0506ora1744.txt";
+    
     // Inizializza e apri il file di input
     ifstream file_in(file_name);
  
@@ -29,7 +30,6 @@ void rapporto(double &r1, double &r2, double &r3, double &r4) {
     
     // Loop fino alla fine del file, riga per riga
     while (file_in >> q1_l >> q1_h >> q2_l >> q2_h >> q3_l >> q3_h >> q4_l >> q4_h >> t1 >> t2 >> t3 >> t4 >> clock) {
-
         // Ignora le righe con valori nulli
         if (q1_l == 0 || q2_l == 0 || q3_l == 0 || q4_l == 0 || t1 == 0 || t2 == 0 || t3 == 0 || t4 == 0) {
             continue;
@@ -48,18 +48,34 @@ void rapporto(double &r1, double &r2, double &r3, double &r4) {
     // Chiudi il file di input
     file_in.close();
     
-    // Calcola i rapporti medi
-    r1 = q1_sum / icand ;
-    r2 = q2_sum / icand ;
-    r3 = q3_sum / icand ;
-    r4 = q4_sum / icand ;
+    // Verifica se icand è diverso da zero per evitare divisioni per zero
+    if (icand > 0) {
+        // Calcola i rapporti medi
+        r1 = q1_sum / icand;
+        r2 = q2_sum / icand;
+        r3 = q3_sum / icand;
+        r4 = q4_sum / icand;
+    } else {
+        // Imposta a zero i rapporti se non ci sono dati validi
+        r1 = r2 = r3 = r4 = 0;
+    }
     
     // Stampa i risultati
     cout << fixed << setprecision(3);
-    cout << "Rapporto 1: " << r1 << endl;
-    cout << "Rapporto 2: " << r2 << endl;
-    cout << "Rapporto 3: " << r3 << endl;
-    cout << "Rapporto 4: " << r4 << endl;
-    cout << "Numero di candidati: " << icand << endl;
+    cout << "Rapporto1: " << r1 << endl;
+    cout << "Rapporto2: " << r2 << endl;
+    cout << "Rapporto3: " << r3 << endl;
+    cout << "Rapporto4: " << r4 << endl;
+    cout << "Numero_candidati: " << icand << endl;
+
+    // Stampa i risultati su file di output
+    ofstream outFile("../Dati/QDC/rapporti.txt");       // Create an output file
+    if (!outFile.is_open()) {
+        cerr << "Unable to open output file!" << endl;  // Error message if output file can't be opened
+        return;
+    }
+    outFile << setprecision(4) << fixed;                // Set precision to 4 significant digits
+    outFile << "r1\t r2\t r3\t r4\t n_cand\n" << r1 << "   " << r2 << "   " << r3 << "   " << r4 << "   " << icand << endl;
+    outFile.close();                                    // Close the low resolution output file
 
 }
